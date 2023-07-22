b90a568e08b639d58f2657fc438e3e9fc19899e2
ahmedsaif2002 <ahmedsaif2002@gmail.com>
2023-07-22 11:52:15 +0100

    update

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Yay Node!");
+  res.send("Yay Node! Ahmed here");
 });
+
 
 app.listen(3000, function () {
   console.log("Server is listening on port 3000. Ready to accept requests!");
